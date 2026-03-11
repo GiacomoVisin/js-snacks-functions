a41a8deaabf4c2aa10d908fa401b646619561f74
GiacomoVisin <killerboss1150@gmail.com>
2026-03-11 10:37:19 +0100

    fixing task 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,7 +5,7 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function newArray() {
+function newArray(arr) {                  // function declaration
     const newNames = []
 
     for (let i = 0; i < names.length; i++){
@@ -13,6 +13,19 @@ function newArray() {
     }
     return newNames
 }
+
+
+const newArray = (arr) => {
+     const newNames = []                        // function arrow
+
+    for (let i = 0; i < names.length; i++){
+        newNames.push(names[i][0])
+    }
+    return newNames
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 newArray(names)
