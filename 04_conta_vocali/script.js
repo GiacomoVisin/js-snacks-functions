69907e31b0f57ef38174678479c7635df0a38a06
GiacomoVisin <killerboss1150@gmail.com>
2026-03-11 15:07:50 +0100

    task n4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,38 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vocalString(string) {
+    const vocal = [`a`, `e`, `i`, `o`, `u`]
+    let count = 0;                                  // declaration function
+    for (let i = 0; i < string.length; i++) {
+        if (vocal.includes(string[i].toLowerCase())) {
+            count++
+        }
+    }
+    return count
+}
+
+
+
+const vocalString = (string) => {
+    const vocals = [`a`, `e`, `i`, `o`, `u`]
+    let count2 = 0                                          // function arrow
+    for (let i = 0; i < string.length; i++) {
+        if (vocals.includes(string[i].toLowerCase())) {
+            count2++
+        }
+    }
+    return count2
+}
+
+
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+vocalString(word)
+console.log(vocalString(word));
 
 
 
