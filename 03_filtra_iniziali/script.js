2b80b58858f2bd0dbc269e7f6c886defda6e161d
GiacomoVisin <killerboss1150@gmail.com>
2026-03-11 11:58:15 +0100

    task 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,8 +5,40 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstChar (array,character){
+    const firstCharacterNames= []
+                                                                        // function declaration
+    for (let i = 0; i < array.length; i++){
+        if(array[i][0].toUpperCase() === character.toUpperCase()){
+            firstCharacterNames.push(array[i])
+        }
+    }
+          return firstCharacterNames
+}
+
+
+const firstChar2 = (array2,character) => {
+    const firstCharacterNames = []
+    for (let i=0; i < array2.length; i++){
+        if (array2[i][0].toUpperCase() === character.toUpperCase()){
+            firstCharacterNames.push(array2[i])
+        }
+    }
+        return firstCharacterNames
+}
+
+
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
+firstChar(names,`A`)
+console.log(firstChar(names,`A`));
+
+firstChar2(names,`L`)
+console.log(firstChar2(names,`L`));
+
 
 
 
