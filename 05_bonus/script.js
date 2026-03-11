76e840c047e0edd8d8de7839005b03c450ee88a1
GiacomoVisin <killerboss1150@gmail.com>
2026-03-11 15:22:28 +0100

    task bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,13 +5,34 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const name2 = 'Mario';
 
 
 // Dichiara la funzione qui.
+function helloNamebyHour(name2) {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting = ``;
 
+    if (hour <= 13) {
+        greeting = `Buongiorno`
+        console.log(`${greeting} ${name2}`);
+    }
+    else if (hour > 13 && hour <= 17) {
+        greeting = `Buon pomeriggio`
+        console.log(`${greeting} ${name2}`);
+    }
+    else {
+        greeting = `Buonasera`
+        console.log(`${greeting} ${name2}`);
+    }
+    return (`${greeting} ${name2}`)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+helloNamebyHour(name2)
+console.log(helloNamebyHour(name2));
+
 
 
 
